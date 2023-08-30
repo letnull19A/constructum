63151ef5f676a0e180e21d58b6cb023d6f3c009a
letnull19a <alexvolkovdd@gmail.com>
2023-08-30 22:00:14 +0400

    дфые срфтпуы

--- client/src/pages/page.project-builded/BuildedProject.tsx
+++ client/src/pages/page.project-builded/BuildedProject.tsx
@@ -17,10 +17,11 @@ export const BuildedProject = () => {
                     <CardContent>
                         <code className='code-block'>
                             <pre>
-                            &#91;Table&#40;entity&#41;&#93;<br/>
+                            &#91;Table&#40;&#34;entity&#34;&#41;&#93;<br/>
                             <span className='modify'>public</span> <span className='sealed'>sealed</span> class <span className='type'>Entity</span><br/>
                             &#123;<br/>
                             &#9;&#91;Key&#93;<br/>
+                            &#9;&#91;Column&#40;&#34;entity_id&#34;&#41;&#93;<br/>
                             &#9;<span className='modify'>public</span> <span className='type'>Guid</span> Id &#123; get; set; &#125;
                             <br/>&#125;
                             </pre>
